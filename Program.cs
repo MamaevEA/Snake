fbb95a7932853afbcdb56e9709958724afb7e7ac
MamaevEA <mamaevea@gmail.com>
2022-09-14 13:25:48 +0500

    Start

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Игра "Змейка" из курса "Основы ООП"
+
+static void Main(string[] args)
+{
+    int x1 = 1;
+    int y1 = 3;
+    char sym1 = '*';
+    Console.SetCursorPosition(x1, y1);
+    Console.Write(sym1);
+    Console.ReadLine();
+}
