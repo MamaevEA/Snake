3570378ea2b68a23724ee16575051717574668a9
MamaevEA <mamaevea@gmail.com>
2022-09-15 08:43:04 +0500

    Add Point and Draw

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-﻿// Игра "Змейка" из курса "Основы ООП"
+﻿
+Point p1 = new Point(1,3,'*');
+p1.Draw();
 
-static void Main(string[] args)
-{
-    int x1 = 1;
-    int y1 = 3;
-    char sym1 = '*';
-    Console.SetCursorPosition(x1, y1);
-    Console.Write(sym1);
-    Console.ReadLine();
-}
+Console.ReadLine();
+
